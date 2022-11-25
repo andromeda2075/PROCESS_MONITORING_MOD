2b71d28658d9a86f8d0b9e3e624d89de6613658e
G <lergiomr@gmail.com>
2022-11-25 16:19:47 -0500

    corrigiendo la documentacion

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
-#!/usr/bin/python3
+''' Programa principal. Se llaman a los módulos creados 
+    para el monitoreo de procesos y salud del sistema.
+'''
 
-#import process_monitor 
-import process_monitor_mod 
 import repository 
 import configuration
 import monitor_system
+import process_monitor_mod 
 
-
-## LECTURA DE LA CONFIGUARACION DESDE UN ARCHIVO JSON
+''' Creación del objeto configuración '''
 config = configuration.Configuration()
 
-##SE CREA UN REPOSITORIO QUE GUARDA DATA EN UNA BD SQLITE
+''' Creación del objeto repositorio '''
 new_repository=repository.SqliteRepository(config.getDbFile(),config.isRingBase(),config.getMaxRegisters())
 
-#--------------------------------------------------------------------------------
-
-##SE CREA EL MONITOR DE PROCESOS
-#process_monitor=process_monitor.ProcessMonitor()
+''' Creación del objeto monitor de procesos '''
 process_monitor_mod=process_monitor_mod.ProcessMonitor()
 
-#process_monitor.SetConfiguration(new_repository,config.getProcessesPeriodVerification(),config.getMaxProcessRam(),config.getMaxProcessCPU())
 process_monitor_mod.SetConfiguration(new_repository,config.getProcessesPeriodVerification(),config.getMaxProcessRam(),config.getMaxProcessCPU())
 
-##SE AGREGAN LOS PROCESOS A MONITOREAR
+''' Se pasa la lista de los procesos dados para el monitoreo '''
 for process in config.getProcesses():
-    #process_monitor.add_monitored(process['name'],process['processes_period_loging'],process['monitoring_children'])
     process_monitor_mod.add_monitored(process['name'],process['processes_period_loging'],process['monitoring_children'])
-##SE INICIA EL MONITOREO
-#process_monitor.start()
+    
+''' Inicio del monitoreo '''
 process_monitor_mod.start()
 
-############## INFORMACIÓN DE LA PC #####################
-
+''' Inicio de la salud del sistema (PC) '''
 pc_info=monitor_system.SystemInfo()
 
 pc_info.PCsetConfiguration(new_repository,config.getMaxDisk(),config.getMaxCPU(),config.getMaxRam(),config.getPcPeriodVerification(),config.getPcPeriodLoging())
 
-## SE INICIA LA OBTENCIÒN DE LA INFORMACION DE LA PC
-pc_info.start() # start invoca a run que es parte del metodo de threading
+# El método start invoca inplícitamente al método run.
+# El método run es parte de la libreria threading
+pc_info.start() 
