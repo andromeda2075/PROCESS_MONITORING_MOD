0b196aaf9be993bb128a160f43a10455c0ca889f
Carlos Ynoquio <cynoquio@varayoc.lab>
2022-12-14 17:24:54 -0500

    Se corrige sentencia sql de insercion de data. Se permite especificar la carpeta a procesar como parametro por linea de comando. Se agrega archivo conteniendo queries para obtener reportes

--- migrate_pchealth.py
+++ migrate_pchealth.py
@@ -3,9 +3,10 @@ import time
 import datetime
 import os        
 import mysql.connector
-
+import sys
+ 
 '''
-CREATE TABLE pc(id int NOT NULL AUTO_INCREMENT, node_name varchar(30),process_name VARCHAR(30),timestamp_occured TIMESTAMP,event varchar(20), pid INT, cpu_percent FLOAT, memory_Mb FLOAT, PRIMARY KEY (id)) 
+CREATE TABLE pc(id int NOT NULL AUTO_INCREMENT, node_name varchar(30),cpu_used float , memory_used float , disk_used int , status_pc varchar(30),core_temperature text, core_status_temperature text ,timestamp_occured TIMESTAMP, PRIMARY KEY (id));
 '''
 class SqliteRepository:
     hostname=""
@@ -15,18 +16,17 @@ class SqliteRepository:
     conMysql=0
     curMysql = 0
 
-    sqlInsertProcesses ="insert into pc(node_name, cpu_used, memory_used, disk_used, status_pc,core_temperature, core_status_temperature,timestamp values (%s,%s,%s,%s,%s,%s,%s, %s)"
+    sqlInsertPc ="insert into pc(node_name, cpu_used, memory_used, disk_used, status_pc,core_temperature, core_status_temperature,timestamp_occured) values (%s,%s,%s,%s,%s,%s,%s, %s)"
 
-    url_prefix = "./" # Ruta de donde se extraerà la base de datos
+    url_prefix = "./backups" # Ruta de donde se extraerà la base de datos
 
     
     def __init__(self):
-        print("hello")
         self.conMysql = mysql.connector.connect(
             host="localhost",
-            user="prueba2022",
-            password="SoporteVarayoc..2022",
-            database="testdata"
+            user="pruebas2022",
+            password="pruebas2022",
+            database="pruebas2022"
         )
 
         self.curMysql = self.conMysql.cursor()
@@ -38,31 +38,39 @@ class SqliteRepository:
         self.dbName = dbfilename
         # if not os.path.exists(self.url_prefix):
         #     os.mkdir(self.url_prefix)
-        self.con = sqlite3.connect(self.url_prefix + self.dbName, check_same_thread=False)
+        self.con = sqlite3.connect(self.dbName, check_same_thread=False)
         self.cur = self.con.cursor()
             
-    def migratePC(self):
-        #TABLA DE SQLITE
+    def migrate(self):
         res = self.cur.execute("SELECT name FROM sqlite_master WHERE name='PC'")
         if res.fetchone() is None:
             print("pc: tabla NO existe")
         else:
             res = self.cur.execute("SELECT * FROM pc")
             for row in res:
-                seconds = int(row[1])
                 # node_name, cpu_used, memory_used, disk_used, status_pc,core_temperature, core_status_temperature,timestamp
-                timestamp = datetime.datetime.fromtimestamp(row[1]).strftime('%Y-%m-%d %H:%M:%S')
-                print(row[5])
+                timestamp = datetime.datetime.fromtimestamp(row[6]).strftime('%Y-%m-%d %H:%M:%S')
                 datos=(self.hostname,row[0],row[1],row[2],row[3],row[4],row[5],timestamp)
-                print(datos)
-                self.curMysql.execute(self.sqlInsertProcesses,datos)
+                #print(datos)
+                self.curMysql.execute(self.sqlInsertPc,datos)
               
             self.conMysql.commit()  
 
+
+
+# total arguments
+n = len(sys.argv)
+if n <= 1 :
+    print("Debe ingresar el directorio a procesar")
+    exit(0)
+
+basepath=sys.argv[1]
+
 transaction =  SqliteRepository()
 
-basepath="./dia12"
-for e in os.listdir(basepath):
+directory_list1 = os.listdir(basepath)
+directory_list1.sort()
+for e in directory_list1:
     if os.path.isdir(os.path.join(basepath,e)):
         for f in os.listdir(os.path.join(basepath,e)):
             if os.path.isdir(os.path.join(basepath,e,f)):
@@ -73,7 +81,7 @@ for e in os.listdir(basepath):
                         unidad=f.split('.')[0]
                         #print(unidad)
                         transaction.setup(os.path.join(basepath,e,f,g), unidad)
-                        transaction.migratePC()
+                        transaction.migrate()
 
 exit(0)
 
--- migrate_processes.py
+++ migrate_processes.py
@@ -3,6 +3,8 @@ import time
 import datetime
 import os        
 import mysql.connector
+import sys
+ 
 
 '''
 CREATE TABLE monitored(id int NOT NULL AUTO_INCREMENT, node_name varchar(30),process_name VARCHAR(30),timestamp_occured TIMESTAMP,event varchar(20), pid INT, cpu_percent FLOAT, memory_Mb FLOAT, PRIMARY KEY (id)) 
@@ -17,16 +19,15 @@ class SqliteRepository:
 
     sqlInsertProcesses ="insert into monitored(node_name,process_name,timestamp_occured,event,pid,cpu_percent,memory_Mb)  values (%s,%s,%s,%s,%s,%s,%s)"
 
-    url_prefix = "./" # Ruta de donde se extraerà la base de datos
+    url_prefix = "./backups" # Ruta de donde se extraerà la base de datos
 
     
     def __init__(self):
-        print("hello")
         self.conMysql = mysql.connector.connect(
             host="localhost",
-            user="prueba2022",
-            password="SoporteVarayoc..2022",
-            database="testdata"
+            user="pruebas2022",
+            password="pruebas2022",
+            database="pruebas2022"
         )
 
         self.curMysql = self.conMysql.cursor()
@@ -38,7 +39,7 @@ class SqliteRepository:
         self.dbName = dbfilename
         # if not os.path.exists(self.url_prefix):
         #     os.mkdir(self.url_prefix)
-        self.con = sqlite3.connect(self.url_prefix + self.dbName, check_same_thread=False)
+        self.con = sqlite3.connect(self.dbName, check_same_thread=False)
         self.cur = self.con.cursor()
             
     def migrate(self):
@@ -57,10 +58,19 @@ class SqliteRepository:
               
             self.conMysql.commit()  
 
+# total arguments
+n = len(sys.argv)
+if n <= 1 :
+    print("Debe ingresar el directorio a procesar")
+    exit(0)
+
+basepath=sys.argv[1]
+
 transaction =  SqliteRepository()
 
-basepath="./dia12"
-for e in os.listdir(basepath):
+directory_list1 = os.listdir(basepath)
+directory_list1.sort()
+for e in directory_list1:
     if os.path.isdir(os.path.join(basepath,e)):
         for f in os.listdir(os.path.join(basepath,e)):
             if os.path.isdir(os.path.join(basepath,e,f)):
@@ -74,7 +84,3 @@ for e in os.listdir(basepath):
                         transaction.migrate()
 
 exit(0)
-
-
-
-
