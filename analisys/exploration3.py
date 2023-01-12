10b3aa9743befd6683568d98f64adbac6b54810d
G <lergiomr@gmail.com>
2023-01-12 16:37:28 -0500

    graficos por cada instancia

--- analisys/exploration3.py
+++ analisys/exploration3.py
@@ -50,28 +50,6 @@ nodes_name=['fm57-01','fm57-02','fm57-03','fm57-04','fm57-05','fm57-06','fm57-07
 main_name_process=['GuiDisplay-run','GuiDisplaySec-run','EcRegMsg','networking','Rms-node-run',
         'GuiRootPanel-run','GuiSoundWarnings-run','Radar-osiris-run','durability','ospl','spliced']
 
-def generate_pd_query(sql_query):
-    mysql_db=mysql.connector.connect(
-            host='localhost',
-            user='prueba2022',
-            password='SoporteVarayoc..2022',
-            database='testdata'
-        )
-    df=pd.read_sql(sql_query,mysql_db)
-    return df
-
-def new_date(time_end):
-    c=time_end
-    new_c=c.split(" ")
-    new_c1=new_c[0].replace('-','/')
-    new_c2=new_c1.split("/")
-    year=new_c2[0][2:4]
-    new_c2[0]=year
-    reverse_new_c2=list(reversed(new_c2))
-    new_date= "/".join(reverse_new_c2)
-    date= new_date+" "+new_c[1]
-    return date
-
 banned_list="('mandb', 'xrandr', 'dpkg', 'fluxbox', 'ssh-agent','sleep', 'sh', 'aterm', 'guishow.sh', 'gzip', 'man-db', 'logrotate', 'apt', 'apt-get', 'run-parts', 'pulseaudio')"
 
 # Consultas
@@ -96,15 +74,101 @@ sql_template3 = '''
 
 sql_template4='''
 select concat(node_name,"_",process_name,"_",pid) as uniquename, cpu_percent,memory_Mb,timestamp_occured,pid,event from monitored where node_name='{node_name}' 
-and process_name='{name_process}' order by timestamp_occured;
+and process_name='{name_process}' and timestamp_occured BETWEEN '{inicio}' AND '{fin}' order by timestamp_occured;
 '''
 sql_template5='''
 select * from pc
 '''
-def query_general(sql_consult,node_name,name_process):
-    query=sql_consult.format(name_process=name_process,node_name=node_name)
+# Métodos
+
+def generate_pd_query(sql_query):
+    mysql_db=mysql.connector.connect(
+            host='localhost',
+            user='prueba2022',
+            password='SoporteVarayoc..2022',
+            database='testdata'
+        )
+    df=pd.read_sql(sql_query,mysql_db)
+    return df
+
+def new_date(time_end):
+    c=time_end
+    new_c=c.split(" ")
+    new_c1=new_c[0].replace('-','/')
+    new_c2=new_c1.split("/")
+    year=new_c2[0][2:4]
+    new_c2[0]=year
+    reverse_new_c2=list(reversed(new_c2))
+    new_date= "/".join(reverse_new_c2)
+    date= new_date+" "+new_c[1]
+    return date
+
+
+def query_general(sql_consult,node_name,name_process,inicio,fin):
+    query=sql_consult.format(name_process=name_process,node_name=node_name,inicio=inicio,fin=fin)
     df=generate_pd_query(query)
     return df
+
+#print(new_df)
+# df_transposed = new_df.T
+# print(df_transposed)
+# ax = plt.axes()
+# fmt = mdates.DateFormatter('%d-%H:%M:%S')
+# ax.xaxis.set_major_formatter(fmt)
+# new_df['event'].plot(marker='.', alpha=0.5, linestyle='None', figsize=(15, 5),color='red')
+# plt.show()
+# consult1=new_df[new_df['uniquename']=='fm57-c01a_GuiDisplay-run_1154']
+# print(consult1)
+# consult2=new_df[new_df['uniquename']=='fm57-c01a_GuiDisplay-run_1172']
+# print(consult2)
+# ax = plt.axes()
+# fmt = mdates.DateFormatter('%d-%H:%M:%S')
+# ax.xaxis.set_major_formatter(fmt)
+# ax.set_xlim([datetime(2022,12,12,14,0,0), datetime(2022,12,15,4,30,00)])
+# yticks = np.arange(0, 4, 1)
+# yrange = (yticks[0], yticks[-1])
+# ax.set_ylabel('Event')
+# ax.set_yticks(yticks)
+# ax.set_ylim(yrange)
+# consult['event'].plot(color='red')
+# plt.show()
+
+
+def create_dataframe(df,procces):
+    names=new_df["uniquename"].unique()
+    for name in names:
+        consult=df[df['uniquename']==name]
+        pid = consult.iloc[0,2]
+        event = 3
+        consultFinal = consult
+        for index, aa in consult.iterrows():
+            if aa[1] !=0 and event ==0:
+                index1= index - timedelta(seconds=1)
+                consultFinal.loc[index1] = aa[0], 0, aa[2]
+                consultFinal = consultFinal.sort_index()
+            event = aa[1]
+        ax = plt.axes()
+        fmt = mdates.DateFormatter('%d-%H:%M:%S')
+        ax.xaxis.set_major_formatter(fmt)
+        yticks = np.arange(0, 4, 1)
+        yrange = (yticks[0], yticks[-1])
+        ax.set_xlabel('timestamp_occured',fontsize = 12, fontweight ='bold')
+        ax.set_ylabel('Event',fontsize = 13, fontweight ='bold')
+        ax.set_yticks(yticks)
+        ax.set_ylim(yrange)
+        font_dict = {'fontsize': 10, 'fontweight': 'bold','verticalalignment':'top'}
+        ax.set_yticklabels(['fail','warning','running','start'],fontdict=font_dict)
+        ax.grid(True)
+        plt.xticks(rotation=45)
+        plt.plot(consultFinal['event'],label=pid,color='red')
+    
+        plt.legend(title='PIDs')
+        plt.title(procces,fontsize=14,fontweight ='bold')
+        # plt.savefig(procces)
+        plt.show()
+
+    
+
  # PARAMETROS
 
 row=0
@@ -125,41 +189,20 @@ cpu_column='cpu_percent'
 2:running
 1:warning
 0:fail
-
 '''
+plt.margins(0.2)
 
-df=query_general(sql_template4,nodes_name[11],main_name_process[0])
-print(df)
-new_df=df.loc[:, ['uniquename', 'timestamp_occured','event']]
-new_df.set_index('timestamp_occured', inplace = True)
-new_df['event'].replace(['start', 'running','warning','fail'],[3, 2,1,0], inplace=True)
-print(new_df)
-df_transposed = new_df.T
-print(df_transposed)
-# ax = plt.axes()
-# fmt = mdates.DateFormatter('%d-%H:%M:%S')
-# ax.xaxis.set_major_formatter(fmt)
-# new_df['event'].plot(marker='.', alpha=0.5, linestyle='None', figsize=(15, 5),color='red')
-# plt.show()
-consult1=new_df[new_df['uniquename']=='fm57-c01a_GuiDisplay-run_1154']
-print(consult1)
-consult2=new_df[new_df['uniquename']=='fm57-c01a_GuiDisplay-run_1172']
-print(consult2)
-# ax = plt.axes()
-# fmt = mdates.DateFormatter('%d-%H:%M:%S')
-# ax.xaxis.set_major_formatter(fmt)
-# ax.set_xlim([datetime(2022,12,12,14,0,0), datetime(2022,12,15,4,30,00)])
-# yticks = np.arange(0, 4, 1)
-# yrange = (yticks[0], yticks[-1])
-# ax.set_ylabel('Event')
-# ax.set_yticks(yticks)
-# ax.set_ylim(yrange)
-# consult['event'].plot(color='red')
-# plt.show()
+for date in [['2022-12-12','2022-12-13'],['2022-12-13','2022-12-14'],['2022-12-14','2022-12-15']]:
+    for procces in  main_name_process:
 
-def create_dataframe(df):
-    df_new = pd.DataFrame()
-    df_new['timestamp'] = df['Josy', 'Vaughn', 'Neale', 'Teirtza']
-    for name in df['uniquename']:
-        consult=df[df['uniquename']=name]
-    pass
+        df=query_general(sql_template4,nodes_name[11],procces,date[0],date[1])
+        new_df=df.loc[:, ['uniquename', 'timestamp_occured','event', 'pid']]
+        new_df.set_index('timestamp_occured', inplace = True)
+        new_df['event'].replace(['start', 'running','warning','fail'],[3, 2,1,0], inplace=True)
+        
+        create_dataframe(new_df,procces)
+
+
+
+
+
--- analisys/test.py
+++ analisys/test.py
@@ -2,6 +2,7 @@ import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 
+'''
 d2 = {'name': ['Alice', 'Bob','Guiomar'],
 
       'score': [9.5, 8,10],
@@ -11,6 +12,11 @@ d2 = {'name': ['Alice', 'Bob','Guiomar'],
       'kids': [0, 0,1]}
 df2 = pd.DataFrame(data=d2)
 print(df2)
+
+
 df = pd.DataFrame()
 df['name'] = d2['name']
 print(df)
+
+'''
+
