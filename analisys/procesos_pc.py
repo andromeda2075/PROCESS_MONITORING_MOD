bdbc23eae55cebd42f6e72e3ad30191a2fa84d3f
G <lergiomr@gmail.com>
2023-01-31 16:32:01 -0500

    pc table

---
+++ analisys/procesos_pc.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time     
+import mysql.connector
+import sys
+from datetime import datetime, timedelta
+import string
+import matplotlib.dates as mdates
+import seaborn as sns
+from datetime import datetime
+
